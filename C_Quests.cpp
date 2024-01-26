b045b2dfa5ba5cec214da1437443a471a793f33a
aryankad1an <aryankaddian842@gmail.com>
2024-01-27 01:17:15 +0530

    1

--- C_Quests.cpp
+++ C_Quests.cpp
@@ -1,9 +1,10 @@
+// link: https://codeforces.com/contest/1914/problem/C
 #include <iostream>
 
 using namespace std;
-void solve(int n, int k, int q[], int e[]){
+
     
-}
+
 
 int main() {
     int t;
@@ -18,7 +19,36 @@ int main() {
         for(int i = 0; i < n; i++){
             cin >> e[i];
         }
-        solve(n, k, q, e);
+        int ans = 0;
+        int allowedretries = 0;
+        if(k > n){
+            allowedretries = k-n;
+            k = n;
+        }
+
+        for(int i = k-1; i >= 0; i--){
+            int temp_sum = 0;
+
+            // sum of 0 to i elements of q[n]
+            for(int j = 0; j <= i; j++){
+                temp_sum += q[j];
+            }
+
+            // finding the max of e[n] from 0 to i;
+            int max_e = 0;
+            for(int j = 0; j <= i; j++){
+                if(e[j] > max_e){
+                    max_e = e[j];
+                }
+            }
+
+            temp_sum += max_e*allowedretries;
+            if(temp_sum > ans){
+                ans = temp_sum;
+            }
+            allowedretries += 1;
+        }
+        cout << ans << endl;
     }
     return 0;
 }
