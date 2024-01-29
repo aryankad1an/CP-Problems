a0e59e286acf696ce8052456658a5a638c07725d
aryankad1an <aryankaddian842@gmail.com>
2024-01-29 18:28:52 +0530

    c

--- D_Jumping_Through_Segments.cpp
+++ D_Jumping_Through_Segments.cpp
@@ -5,18 +5,10 @@
 #include<vector>
 #include<map>
 #include<set>
-#include<queue>
-#include<stack>
 #include<algorithm>
-#include<cmath>
-#include<cstring>
-#include<cstdio>
-#include<cstdlib>
-#include<ctime>
-#include<cassert>
 
 using namespace std;
 int main(){
     
     return 0;
-}
+}       
