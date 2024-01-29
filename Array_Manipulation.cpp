dc26e7fa3d5908f97e76d8dc4355a7fe9e4293df
aryankad1an <aryankaddian842@gmail.com>
2024-01-30 00:58:30 +0530

    optimized :)

--- Array_Manipulation.cpp
+++ Array_Manipulation.cpp
@@ -17,11 +17,34 @@ int main(){
     while(m--){
         int a, b, d;
         cin >> a >> b >> d;
-        for (int i = a; i <= b; i++)
-        {
-            arr[i] += d;
-        }
+        // causes TLE
+        // for (int i = a; i <= b; i++)
+        // {
+        //     arr[i] += d;
+        // }
+
+        // Optimized Solution
+        arr[a] += d;
+        arr[b+1] -= d;
     }
+    /*
+    Concept Used:
+
+    0 0 0 0 0 : Initial Array
+    2 4 5 : Operation 1
+    0 5 5 5 0 : After Operation 1 (1 based indexing)
+    Now this array can be represented as
+    0 5 0 0 -5
+    Prefix Array of the above array is 
+    0 5 5 5 0 which returns the same array after performing the operation
+    */
+
+    // changing the array to prefix array
+    for (int i = 1; i <= n; i++)
+    {
+        arr[i] += arr[i-1];
+    }
+    // finding the maximum element in the array
     long max = -1;
     for (int i = 0; i < n; i++)
     {
