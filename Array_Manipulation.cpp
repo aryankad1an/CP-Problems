5316a808187445afa0bdbacd00fc2fe8d84c3e0c
aryankad1an <aryankaddian842@gmail.com>
2024-01-30 00:53:11 +0530

    tle :(

--- Array_Manipulation.cpp
+++ Array_Manipulation.cpp
@@ -1,5 +1,5 @@
 // Link To Problem : https://www.hackerrank.com/challenges/crush/problem
-
+// We Start with an array of zeros and we have to perform n operations on it
 
 #include<iostream>
 #include<vector>
@@ -7,9 +7,28 @@
 #include<set>
 #include<algorithm>
 #define long long long int
+const int N = 1e7+10;
+long arr[N];
 
 using namespace std;
 int main(){
-    
+    int n, m;
+    cin >> n >> m;
+    while(m--){
+        int a, b, d;
+        cin >> a >> b >> d;
+        for (int i = a; i <= b; i++)
+        {
+            arr[i] += d;
+        }
+    }
+    long max = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    cout << max << endl;
     return 0;
 }
