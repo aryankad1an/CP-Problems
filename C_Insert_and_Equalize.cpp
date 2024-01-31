a7952d6111743be2248a434ab6616171830e5a33
aryankad1an <aryankaddian842@gmail.com>
2024-01-31 14:53:32 +0530

    c

---
+++ C_Insert_and_Equalize.cpp
@@ -0,0 +1,15 @@
+// Link To Problem : https://codeforces.com/contest/1902/problem/C
+
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#define long long long int
+
+using namespace std;
+int main(){
+    
+    return 0;
+}
