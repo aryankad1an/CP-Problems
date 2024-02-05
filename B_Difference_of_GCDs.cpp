687d547da7f307e1d076ead1dcc4c1250501a56a
aryankad1an <aryankaddian842@gmail.com>
2024-02-05 11:23:47 +0530

    c

---
+++ B_Difference_of_GCDs.cpp
@@ -0,0 +1,15 @@
+// Link To Problem : https://codeforces.com/problemset/problem/1708/B
+
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#define long long long int
+
+using namespace std;
+int main(){
+    
+    return 0;
+}
